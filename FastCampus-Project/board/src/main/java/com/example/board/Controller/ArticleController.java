390d099d1dea1b5864f4429fe47607a0d7d66241
dongwoo <kimdongwoo0930@gmail.com>
2023-01-05 16:22:48 +0900

    게시판 페이지네이션기능 및 세부기능 화면 구현

--- FastCampus-Project/board/src/main/java/com/example/board/Controller/ArticleController.java
+++ FastCampus-Project/board/src/main/java/com/example/board/Controller/ArticleController.java
@@ -4,7 +4,9 @@ import com.example.board.Dto.ArticleResponse;
 import com.example.board.Dto.ArticleWithCommentResponse;
 import com.example.board.domain.type.SearchType;
 import com.example.board.service.ArticleService;
+import com.example.board.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 
 /**
@@ -29,6 +33,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
 
     @GetMapping
@@ -36,8 +41,13 @@ public class ArticleController {
             @RequestParam(required = false) SearchType searchType,
             @RequestParam(required = false) String searchValue,
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
-            ModelMap modelMap){
-        modelMap.addAttribute("articles" , articleService.searchArticles(searchType,searchValue,pageable).map(ArticleResponse::from));
+            ModelMap map){
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
---
+++ FastCampus-Project/board/src/test/java/com/example/board/service/PaginationServiceTest.java
@@ -0,0 +1,61 @@
+package com.example.board.service;
+
+import com.example.board.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@DisplayName("비지니스 로직 - 페이지네이션")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE, classes = PaginationService.class)
+class PaginationServiceTest {
+    private final PaginationService sut;
+
+    public PaginationServiceTest(@Autowired PaginationService paginationService) {
+        this.sut = paginationService;
+    }
+
+    @DisplayName("현재 페이지 변호와 총 페이지 수를 주면, 페이징 바 리스트를 만들어준다.")
+    @MethodSource
+    @ParameterizedTest(name = "[{index}] {0} , {1} => {2}")
+    void givenCurrentPageNumberAndTotalPages_whenCalculating_thenReturnPaginationBarNumber(int currentPageNumber, int totalPages, List<Integer> expected){
+        // Given
+
+        // When
+        List<Integer> actual =  sut.getPaginationBarNumbers(currentPageNumber,totalPages);
+        // Then
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> givenCurrentPageNumberAndTotalPages_whenCalculating_thenReturnPaginationBarNumber(){
+        return Stream.of(
+                arguments(0,13,List.of(0,1,2,3,4)),
+                arguments(1,13,List.of(0,1,2,3,4)),
+                arguments(2,13,List.of(0,1,2,3,4)),
+                arguments(3,13,List.of(1,2,3,4,5)),
+                arguments(4,13,List.of(2,3,4,5,6)),
+                arguments(5,13,List.of(3,4,5,6,7)),
+                arguments(10,13,List.of(8,9,10,11,12)),
+                arguments(9,13,List.of(7,8,9,10,11))
+        );
+    }
+
+    @Test
+    void given_when_then(){
+        // Given
+
+        // When
+        int barLength = sut.currentBarLength();
+        // Then
+        assertThat(barLength).isEqualTo(5);
+    }
+}
