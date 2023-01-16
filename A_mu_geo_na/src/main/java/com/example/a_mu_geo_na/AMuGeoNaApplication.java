8c969d7db0f56e6627611bd64f828211d5c7fe17
dongwoo <kimdongwoo0930@gmail.com>
2023-01-16 12:02:56 +0900

    아무거나 프로젝트 생성 완료

---
+++ A_mu_geo_na/src/main/java/com/example/a_mu_geo_na/AMuGeoNaApplication.java
@@ -0,0 +1,13 @@
+package com.example.a_mu_geo_na;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMuGeoNaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AMuGeoNaApplication.class, args);
+	}
+
+}
---
+++ A_mu_geo_na/src/test/java/com/example/a_mu_geo_na/AMuGeoNaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.a_mu_geo_na;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AMuGeoNaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
